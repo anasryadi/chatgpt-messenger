0b7021e95011dfb770a381e455acf8e04da78192
anasryadi <anasryadi0@gmail.com>
2023-03-30 13:57:21 -0700

    installed heroicons and added an example icon

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
+import { BeakerIcon } from '@heroicons/react/24/solid'
+
 function HomePage() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen text-white">
+    <div className="flex flex-col items-center justify-center h-screen px-2 text-white">
       <h1 className="text-5xl font-bold mb-20">ChatGPT</h1>
       <div>
         <div>
           <div className="flex flex-col items-center justify-center mb-5">
             {/* Sun Icon */}
+            <BeakerIcon className="h-6 w-6 text-blue-500"/>
             <h2>Examples</h2>
           </div>
           <div className="space-y-2">
